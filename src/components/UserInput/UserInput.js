7a46f345bc44e764479cddc99d7674c40a554e09
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-21 22:35:13 +0300

    feat: add a clearing of input fields after adding a user

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -17,17 +17,19 @@ const UserInput = (props) => {
     evt.preventDefault();
     const user = { name: username, age: age, id: Math.random().toString() };
     props.onAddUser(user);
+    setUserName("");
+    setAge("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div>
         <label>Username</label>
-        <input type="text" name="username" onChange={UserInputHandler} />
+        <input type="text" name="username" value={username} onChange={UserInputHandler} />
       </div>
       <div>
         <label>Age (Years)</label>
-        <input type="number" name="age" onChange={AgeInputHandler} />
+        <input type="number" name="age" value={age} onChange={AgeInputHandler} />
       </div>
       <Button type="submit">Add User</Button>
     </form>
