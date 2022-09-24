16adbfcd18b47d3a709b213b69b8fb256354d98c
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-24 15:24:11 +0300

    feat: add styling for button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,11 @@
+import styles from "./Button.module.css";
+
 const Button = (props) => {
   return (
-    <button type={props.type} onClick={props.onClick}>
+    <button className={styles.button} type={props.type} onClick={props.onClick}>
       {props.children}
     </button>
-  )
+  );
 };
 
 export default Button;
