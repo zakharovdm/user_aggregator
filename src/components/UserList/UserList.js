2a7df207605f0eea56a63c76e8b7fd51130bb249
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-27 22:44:22 +0300

    feat: add styling for userList component

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,16 +1,20 @@
 import User from "../User/User";
+import Card from "../UI/Card/Card";
+import styles from "./UserList.module.css";
 
 const UserList = (props) => {
   return (
-    <ul>
-      {props.users.map((user) => {
-        return (
-          <User key={user.id} id={user.id} onDelete={props.onDeleteUser}>
-            {`${user.name} (${user.age} years old)`}
-          </User>
-        );
-      })}
-    </ul>
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <User key={user.id} id={user.id} onDelete={props.onDeleteUser}>
+              {`${user.name} (${user.age} years old)`}
+            </User>
+          );
+        })}
+      </ul>
+    </Card>
   );
 };
 
